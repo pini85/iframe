917ae1cdab5b6fe8b1abc66345d399492754daa7
Pinchas Hodadad <pinchas.hodadad@gmail.com>
2021-08-29 12:05:30 +0300

    a

--- src/IFrame.js
+++ src/IFrame.js
@@ -2,6 +2,7 @@ const IFrame = ({ url }) => {
   const style = {
     height: "100vh",
   };
+  console.log("hi");
   return (
     <div style={style}>
       <iframe src={url} height="100%" width="100%" />
